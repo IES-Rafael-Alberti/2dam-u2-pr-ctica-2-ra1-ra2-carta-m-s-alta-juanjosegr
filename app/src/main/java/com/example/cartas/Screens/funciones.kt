65ea645c57293b828149b46e1e8069ef362bbd7a
juanj <juanjosegr9@gmail.com>
2023-11-14 14:14:29 +0100

    context y remember

--- app/src/main/java/com/example/cartas/Screens/funciones.kt
+++ app/src/main/java/com/example/cartas/Screens/funciones.kt
@@ -1,8 +1,18 @@
 package com.example.cartas.Screens
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
 
 @Composable
-fun inicio(){
+fun inicio() {
+
+    val context = LocalContext.current
+    var cartaBocaAbajo by rememberSaveable { mutableStateOf("abajo") }
+    var cartaBocaArriba by rememberSaveable { mutableStateOf("") }
+
 
 }
