350fc92291b2b264e4c457cdf0f0c494b3cdd96f
juanj <juanjosegr9@gmail.com>
2023-11-14 13:54:43 +0100

    Cartas y inicio del programa

--- app/src/main/java/com/example/cartas/MainActivity.kt
+++ app/src/main/java/com/example/cartas/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.example.cartas.Screens.inicio
 import com.example.cartas.ui.theme.CartasTheme
 
 class MainActivity : ComponentActivity() {
